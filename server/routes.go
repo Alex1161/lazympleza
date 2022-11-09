ac3ef3f620ca399c4e4c3c1d4bd09750e7a1f0e1
Alex <garbieto@fi.uba.ar>
2022-11-09 18:51:49 -0300

    añadiendo captura de parametros en get

--- server/routes.go
+++ server/routes.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,6 +12,15 @@ func notSupportedMethod(w http.ResponseWriter, r *http.Request) {
 }
 
 func initRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			notSupportedMethod(w, r)
+			return
+		}
+		// Ejemplo de como catchear los parametros
+		log.Println(r.URL.Query()["var"])
+	})
+
 	http.HandleFunc("/wc/cups", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			notSupportedMethod(w, r)
