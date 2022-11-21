8ea71cc5411da3be9e599076d75276624bae64a9
floritfran <fflorit@fi.uba.ar>
2022-11-21 17:56:25 -0300

    Imprime por terminal los ganadores de cada instancia

--- decisionTree/predictions.go
+++ decisionTree/predictions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"lazympleza/logic"
 	"lazympleza/memoize"
+	"strconv"
 )
 
 func SimulateGroup(group string) (string, string) {
@@ -72,6 +73,11 @@ func SimulateGroup(group string) (string, string) {
 		}
 	}
 
+	fmt.Println("")
+	fmt.Println("Grupo " + group + ":")
+	fmt.Println(first + " | " + strconv.FormatInt(int64(firstPoints), 10) + "pts")
+	fmt.Println(second + " | " + strconv.FormatInt(int64(secondPoints), 10) + "pts")
+
 	return first, second
 }
 
@@ -96,20 +102,45 @@ func _GetWinnerWorldCup(args ...string) string {
 	winnerFE, prob := logic.GetWinnerBetween(firstF, secondE)
 	winnerHG, prob := logic.GetWinnerBetween(firstH, secondG)
 
+	fmt.Println("")
+	fmt.Println("---- Octavos de final ----")
+	fmt.Println("El ganador entre " + firstA + " y " + secondB + " es: " + winnerAB)
+	fmt.Println("El ganador entre " + firstC + " y " + secondD + " es: " + winnerCD)
+	fmt.Println("El ganador entre " + firstE + " y " + secondF + " es: " + winnerEF)
+	fmt.Println("El ganador entre " + firstG + " y " + secondH + " es: " + winnerGH)
+	fmt.Println("El ganador entre " + firstB + " y " + secondA + " es: " + winnerBA)
+	fmt.Println("El ganador entre " + firstD + " y " + secondC + " es: " + winnerDC)
+	fmt.Println("El ganador entre " + firstF + " y " + secondE + " es: " + winnerFE)
+	fmt.Println("El ganador entre " + firstH + " y " + secondG + " es: " + winnerHG)
+
 	//Cuartos de final
 	winnerQ1, prob := logic.GetWinnerBetween(winnerAB, winnerCD)
 	winnerQ2, prob := logic.GetWinnerBetween(winnerEF, winnerGH)
 	winnerQ3, prob := logic.GetWinnerBetween(winnerBA, winnerDC)
 	winnerQ4, prob := logic.GetWinnerBetween(winnerFE, winnerHG)
 
+	fmt.Println("")
+	fmt.Println("---- Cuartos de final ----")
+	fmt.Println("El ganador entre " + winnerAB + " y " + winnerCD + " es: " + winnerQ1)
+	fmt.Println("El ganador entre " + winnerEF + " y " + winnerGH + " es: " + winnerQ2)
+	fmt.Println("El ganador entre " + winnerBA + " y " + winnerDC + " es: " + winnerQ3)
+	fmt.Println("El ganador entre " + winnerFE + " y " + winnerHG + " es: " + winnerQ4)
+
 	//Semifinal
 	finalist1, prob := logic.GetWinnerBetween(winnerQ1, winnerQ2)
 	finalist2, prob := logic.GetWinnerBetween(winnerQ3, winnerQ4)
 
+	fmt.Println("")
+	fmt.Println("---- Semifinal ----")
+	fmt.Println("El ganador entre " + winnerQ1 + " y " + winnerQ2 + " es: " + finalist1)
+	fmt.Println("El ganador entre " + winnerQ3 + " y " + winnerQ4 + " es: " + finalist2)
+
 	//Final
 	WCWinner, prob := logic.GetWinnerBetween(finalist1, finalist2)
 
-	//Solo para que no diga no usamos la variable prob
+	fmt.Println("")
+	fmt.Println("---- Final ----")
+	fmt.Println("El ganador entre " + finalist1 + " y " + finalist2 + " es: " + WCWinner)
 	fmt.Println(prob)
 
 	return WCWinner
